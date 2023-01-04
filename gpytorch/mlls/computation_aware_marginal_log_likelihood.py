fixed computation-aware mll with new linear solver interface

gpytorch/mlls/computation_aware_marginal_log_likelihood.py
@@ -38,9 +38,13 @@ class ComputationAwareMarginalLogLikelihood(MarginalLogLikelihood):
                 self.model(*self.model.train_inputs)
             ).covariance_matrix
 
-        repr_weights, Khat_inv_approx, searchdir_sqnorms = self.linear_solver.solve(
+        solver_state = self.linear_solver.solve(
             to_linear_operator(Khat), target  # TODO: do not omit prior mean here
-        )  # TODO: do not do another linear solve in here, rather pass results from solve stored in model, after model(train_x) call
+        )
+        repr_weights = solver_state.solution
+        Khat_inv_approx = solver_state.inverse_op
+        searchdir_sqnorms = solver_state.cache["search_dir_sq_Anorms"]
+        # TODO: do not do another linear solve in here, rather pass results from solve stored in model, after model(train_x) call
 
         # Implementing this via an autograd function is the recommended pattern by
         # PyTorch for extending nn.Module with a custom backward pass.