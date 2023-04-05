bugfix in computation aware prediction

gpytorch/mlls/computation_aware_marginal_log_likelihood.py
@@ -5,7 +5,7 @@ import math
 
 import torch
 from gpytorch import settings
-from linear_operator import to_linear_operator
+from linear_operator import operators, to_linear_operator
 
 from ..likelihoods import GaussianLikelihood, _GaussianLikelihoodBase
 from .marginal_log_likelihood import MarginalLogLikelihood
@@ -84,14 +84,18 @@ class _ComputationAwareMarginalLogLikelihoodFunction(torch.autograd.Function):
             fit_term = ctx.repr_weights.T @ Khat @ ctx.repr_weights
 
             # Computes d/dtheta (\sum_{j=1}^i 1/eta_j d_j' Khat d_j) = \sum_{j=1}^i 1/eta_j d_j' dK/dtheta d_j
-            logdet_term = torch.sum(
-                torch.einsum(
-                    "ni,nn,ni->i",
-                    ctx.prec_approx.root.to_dense(),
-                    Khat,
-                    ctx.prec_approx.root.to_dense(),
+            if isinstance(ctx.prec_approx, operators.ZeroLinearOperator):
+                logdet_term = torch.as_tensor(0.0)
+            else:
+                logdet_term = torch.sum(
+                    torch.einsum(
+                        "ni,nn,ni->i",
+                        ctx.prec_approx.root.to_dense(),
+                        Khat,
+                        ctx.prec_approx.root.to_dense(),
+                    )
                 )
-            )
+
             return 0.5 * (fit_term - logdet_term)
 
         return (

gpytorch/models/exact_prediction_strategies.py
@@ -996,7 +996,7 @@ class ComputationAwarePredictionStrategy(DefaultPredictionStrategy):
         mvn = self.likelihood(self.train_prior_dist, self.train_inputs)
         train_mean, train_train_covar = mvn.loc, mvn.lazy_covariance_matrix
 
-        train_labels_offset = (self.train_labels - train_mean).unsqueeze(-1)
+        train_labels_offset = self.train_labels - train_mean
 
         with torch.no_grad():  # Ensure gradients are not taken through the solve
             self._solver_state = linear_solver.solve(
@@ -1007,7 +1007,7 @@ class ComputationAwarePredictionStrategy(DefaultPredictionStrategy):
         self, inputs, targets, full_inputs, full_targets, full_output, **kwargs
     ):
         raise NotImplementedError(
-            "Fantasy observation updates not yet supported for computation-aware Gaussian processes."
+            "Fantasy observation updates not (yet) supported for computation-aware Gaussian processes."
         )
 
     @property