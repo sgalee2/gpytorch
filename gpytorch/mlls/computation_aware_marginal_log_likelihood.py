065c6779e49e888e800d88f33124b8b22854c1de
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2023-04-21 16:22:53 +0200

    fixed compaware mll

--- gpytorch/mlls/computation_aware_marginal_log_likelihood.py
+++ gpytorch/mlls/computation_aware_marginal_log_likelihood.py
@@ -5,7 +5,7 @@ import math
 from typing import Union
 
 import torch
-from linear_operator import operators, to_linear_operator
+from linear_operator import operators
 
 from ..likelihoods import GaussianLikelihood, _GaussianLikelihoodBase
 from .marginal_log_likelihood import MarginalLogLikelihood
@@ -31,9 +31,8 @@ class ComputationAwareMarginalLogLikelihood(MarginalLogLikelihood):
 
     def forward(self, output: torch.Tensor, target: torch.Tensor, **kwargs):
 
-        Khat = self.likelihood(output).covariance_matrix  # TODO: creates n_train x n_train matrix, does not cause error
-        # Khat = self.likelihood(output).lazy_covariance_matrix.evaluate_kernel() # TODO: causes error (detached from graph?)
-        # Khat = to_linear_operator(self.likelihood(output).covariance_matrix)  # TODO: also causes error
+        # Khat = self.likelihood(output).covariance_matrix  # TODO: creates n_train x n_train matrix, does not cause error
+        Khat = self.likelihood(output).lazy_covariance_matrix.evaluate_kernel()
 
         with torch.no_grad():
             solver_state = self.model.linear_solver.solve(Khat, target)
@@ -48,7 +47,12 @@ class ComputationAwareMarginalLogLikelihood(MarginalLogLikelihood):
         # PyTorch for extending nn.Module with a custom backward pass.
         # See also: https://pytorch.org/docs/stable/notes/extending.html#extending-torch-nn
         return _ComputationAwareMarginalLogLikelihoodFunction.apply(
-            Khat, target, repr_weights, Khat_inv_approx, logdet_estimate
+            Khat.representation_tree(),
+            target,
+            repr_weights,
+            Khat_inv_approx,
+            logdet_estimate,
+            *Khat.representation(),
         )
 
 
@@ -58,12 +62,17 @@ class _ComputationAwareMarginalLogLikelihoodFunction(torch.autograd.Function):
     @staticmethod
     def forward(
         ctx,
-        Khat: Union[torch.Tensor, operators.LinearOperator],
+        Khat_representation_tree,
         y: torch.Tensor,
         repr_weights: torch.Tensor,
         Khat_inv_approx: torch.Tensor,
         logdet_estimate: torch.Tensor,
+        *linear_op_args: torch.Tensor,
     ):
+        # Reconstruct Khat from representation tree
+        Khat = Khat_representation_tree(*linear_op_args)
+
+        # Log marginal likelihood
         lml = -0.5 * (torch.inner(y, repr_weights) + logdet_estimate + Khat.shape[-1] * math.log(2 * math.pi))
 
         ctx.repr_weights = repr_weights
@@ -74,30 +83,24 @@ class _ComputationAwareMarginalLogLikelihoodFunction(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output: torch.Tensor):
-        def _pseudo_lml(Khat):
 
-            # Computes d/dtheta (v' Khat v) = v' dK/dtheta v
-            fit_term = torch.inner(ctx.repr_weights, Khat @ ctx.repr_weights)
-
+        # Create tensor for bilinear forms
+        # Computes d/dtheta (v' Khat v) = v' dK/dtheta v
+        bilinear_form_tensor_list_left = [ctx.repr_weights.reshape(-1, 1)]
+        bilinear_form_tensor_list_right = [ctx.repr_weights.reshape(-1, 1)]
+        if not isinstance(ctx.prec_approx, operators.ZeroLinearOperator):
             # Computes d/dtheta (\sum_{j=1}^i 1/eta_j d_j' Khat d_j) = \sum_{j=1}^i 1/eta_j d_j' dK/dtheta d_j
-            if isinstance(ctx.prec_approx, operators.ZeroLinearOperator):
-                logdet_term = torch.as_tensor(0.0)
-            else:
-                logdet_term = torch.sum(
-                    torch.einsum(
-                        "ni,nn,ni->i",
-                        ctx.prec_approx.root.to_dense(),
-                        Khat,
-                        ctx.prec_approx.root.to_dense(),
-                    )
-                )
+            bilinear_form_tensor_list_left.append(-ctx.prec_approx.root.to_dense())
+            bilinear_form_tensor_list_right.append(ctx.prec_approx.root.to_dense())
 
-            return 0.5 * (fit_term - logdet_term)
+        bilinear_form_tensors_left = 0.5 * torch.cat(bilinear_form_tensor_list_left, -1)
+        bilinear_form_tensors_right = torch.cat(bilinear_form_tensor_list_right, -1)
 
         return (
-            torch.autograd.functional.vjp(_pseudo_lml, ctx.Khat, v=grad_output)[1],
             None,
             None,
             None,
             None,
+            None,
+            *ctx.Khat._bilinear_derivative(bilinear_form_tensors_left, bilinear_form_tensors_right),
         )
