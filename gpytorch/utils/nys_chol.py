f268cae2b54869182e3e0307d62f0c75359e6e8f
AJL-1998 <73225992+AJL-1998@users.noreply.github.com>
2024-08-16 11:18:38 +0100

    Create nys_chol.py

---
+++ gpytorch/utils/nys_chol.py
@@ -0,0 +1,31 @@
+import torch
+
+from .. import settings
+
+def cholesky_helper(matrix, max_iter, alg):
+
+    device = matrix.device
+    n, k = matrix.shape[0], max_iter
+
+    G = torch.empty((k, n), device = device)
+    diags = matrix.diag().detach().clone()
+
+    idx_ = []
+
+    for i in range(k):
+
+        if alg == 'rp':
+            idx = torch.multinomial(diags/torch.sum(diags), 1)
+        elif alg == 'greedy':
+            idx = torch.argmax(diags).item()
+        else:
+            raise RuntimeError("Algorithm {} not recognised".format(alg))
+        
+        idx_.append(idx)
+
+        G[i,:] = (matrix[idx,:] - G[:i,idx].T @ G[:i,:]).evaluate() / torch.sqrt(diags[idx])
+        diags -= G[i,:]**2
+        diags = diags.clip(min=0)
+
+    return G, idx_
+        
