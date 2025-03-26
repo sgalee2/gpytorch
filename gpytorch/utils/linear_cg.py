24db8f42a8c594e14686aad73346d32504d5c841
sgalee2 <73225992+sgalee2@users.noreply.github.com>
2025-03-26 14:44:10 +0000

    Update linear_cg.py

--- gpytorch/utils/linear_cg.py
+++ gpytorch/utils/linear_cg.py
@@ -227,6 +227,7 @@ def linear_cg(
 
     # Start the iteration
     for k in range(n_iter):
+        settings.record_iterates.cg_iterates.append(result)
         # Get next alpha
         # alpha_{k} = (residual_{k-1}^T precon_residual{k-1}) / (p_vec_{k-1}^T mat p_vec_{k-1})
         mvms = matmul_closure(curr_conjugate_vec)
